9e3ac41fb95de9d735e96f94619366b98eb7545d
Vishwas U.S <vishwas.skotegar@gmail.com>
2021-11-30 00:05:32 +0530

    v 0.0.2vs
    models -> userDetails

---
+++ lib/models/user_details/user_detail.dart
@@ -0,0 +1,16 @@
+
+import 'dart:html';
+
+class UserDetails {
+  final String userId;
+  final String userName;
+  final String password;
+  final String userProfilePic;
+  final int phoneNumber;
+  final Geolocation location;
+  final int age;
+  final int xp;
+
+  UserDetails(this.userId, this.userName, this.password, this.userProfilePic, this.phoneNumber, this.location, this.age, this.xp);
+
+}
